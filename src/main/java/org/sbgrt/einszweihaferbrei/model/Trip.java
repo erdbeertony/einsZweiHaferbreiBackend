5490e12a41f496c7299815bfcfaca7529129fd3b
Sebastian Baumgärtner <sebastian.baumgaertner@prodyna.com>
2022-12-14 15:29:59 +0100

    renamed images, added url

--- src/main/java/org/sbgrt/einszweihaferbrei/model/Trip.java
+++ src/main/java/org/sbgrt/einszweihaferbrei/model/Trip.java
@@ -5,6 +5,15 @@ public class Trip {
     String name;
     String description;
 
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    String url;
 
     public String getName() {
         return name;
--- src/main/java/org/sbgrt/einszweihaferbrei/service/TripService.java
+++ src/main/java/org/sbgrt/einszweihaferbrei/service/TripService.java
@@ -11,10 +11,12 @@ public class TripService {
         Trip t1 = new Trip();
         t1.setName("Bungee-Jumping");
         t1.setDescription("Ein unvergessliches Event mit Nervenkitzel");
+        t1.setUrl("https://www.urlaubstracker.de/wp-content/uploads/2017/07/bungee-jump-ausblick.jpg");
 
         Trip t2 = new Trip();
         t2.setName("Protest in China mitmachen");
         t2.setDescription("Tauchen Sie ein in eine Abenteuerwelt ohne Rückkehrgarantie");
+        t2.setUrl("https://media1.faz.net/ppmedia/aktuell/feuilleton/1220981924/1.8493717/format_top1_breit/demonstranten-in-peking.jpg");
 
         return List.of(t1, t2);
     }
