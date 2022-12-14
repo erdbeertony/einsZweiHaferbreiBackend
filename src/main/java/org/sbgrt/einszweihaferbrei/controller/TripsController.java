41956ce6161ec2ae8e29599a6f06cc21f9ffdc98
Sebastian Baumgärtner <sebastian.baumgaertner@prodyna.com>
2022-12-14 16:01:05 +0100

    crossorigin

--- src/main/java/org/sbgrt/einszweihaferbrei/controller/TripsController.java
+++ src/main/java/org/sbgrt/einszweihaferbrei/controller/TripsController.java
@@ -3,6 +3,7 @@ package org.sbgrt.einszweihaferbrei.controller;
 import org.sbgrt.einszweihaferbrei.model.Trip;
 import org.sbgrt.einszweihaferbrei.service.TripService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/trips")
+@CrossOrigin
 //@CrossOrigin //(origins = "http://localhost:4200") // @CrossOrigin(origins = "http://example.com", maxAge = 3600) //
 // @CrossOrigin(origins = {"http://localhost:8383", "http://anotherdomain:4200"})
 public class TripsController {
--- src/main/java/org/sbgrt/einszweihaferbrei/service/TripService.java
+++ src/main/java/org/sbgrt/einszweihaferbrei/service/TripService.java
@@ -21,17 +21,53 @@ public class TripService {
         );
 
         Trip t3  =new Trip(
-                "",
-                "",
-                ""
+                "SpaceX Economy Light Trip",
+                "Weltraumtrip zu ALDI-Preisen dank Reduzierung auf minimale Sicherheitsstandards",
+                "https://www.golem.de/2001/146124-219790-219789_rc.jpg"
         );
 
         Trip t4  =new Trip(
-                "",
-                "",
-                ""
+                "Houserunning",
+                "Heute schon kalte Füße bekommen?",
+                "https://ais.badische-zeitung.de/piece/05/34/7c/5b/87325787-h-720.jpg"
         );
 
-        return List.of(t1, t2);
+        Trip t5  =new Trip(
+                "Fillschirmsprung no-tandem",
+                "Sie haben Lust auf einen Fallschirmsprung, aber keine Lust auf die Sicherung durch einen Tandemmaster?",
+                "https://funsport.pro/images/luftsport/skydive3.jpg"
+        );
+
+//        Trip t5  =new Trip(
+//                "",
+//                "",
+//                ""
+//        );
+//
+//        Trip t5  =new Trip(
+//                "",
+//                "",
+//                ""
+//        );
+//
+//        Trip t5  =new Trip(
+//                "",
+//                "",
+//                ""
+//        );
+//
+//        Trip t5  =new Trip(
+//                "",
+//                "",
+//                ""
+//        );
+//
+//        Trip t5  =new Trip(
+//                "",
+//                "",
+//                ""
+//        );
+
+        return List.of(t1, t2, t3, t4, t5);
     }
 }
