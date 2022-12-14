eaf97de9fcc4662336aa1b88c5d4a4fe558b497e
Sebastian Baumgärtner <sebastian.baumgaertner@prodyna.com>
2022-12-14 15:22:04 +0100

    removes

--- src/main/java/org/sbgrt/orgaappbackend/persistence/WerBringtWasMitRepository.java
+++
@@ -1,9 +0,0 @@
-package org.sbgrt.orgaappbackend.persistence;
-
-import org.sbgrt.orgaappbackend.model.WerBringtWasMit;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-//@Repository
-public interface WerBringtWasMitRepository extends CrudRepository<WerBringtWasMit, Integer> {
-}
