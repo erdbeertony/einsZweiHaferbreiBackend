5c25df750b9ecb2a6d1274e6dc3762cb955a8407
Sebastian Baumgärtner <sebastian.baumgaertner@prodyna.com>
2022-12-15 13:42:44 +0100

    liste vervollständigen

--- src/main/java/org/sbgrt/einszweihaferbrei/service/TripService.java
+++ src/main/java/org/sbgrt/einszweihaferbrei/service/TripService.java
@@ -41,33 +41,33 @@ public class TripService {
         Trip t6  =new Trip(
                 "Panzerfahren",
                 "Beschuss inkl.",
-                ""
+                "https://assets.deutschlandfunk.de/0b222cb2-f10b-4dc9-a6f4-8436509e75d4/1920x1080.jpg?t=1647878364881"
         );
-//
-//        Trip t5  =new Trip(
-//                "",
-//                "",
-//                ""
-//        );
-//
-//        Trip t5  =new Trip(
-//                "",
-//                "",
-//                ""
-//        );
-//
-//        Trip t5  =new Trip(
-//                "",
-//                "",
-//                ""
-//        );
-//
-//        Trip t5  =new Trip(
-//                "",
-//                "",
-//                ""
-//        );
 
-        return List.of(t1, t2, t3, t4, t5);
+        Trip t7  =new Trip(
+                "Workshop Granaten selbst basteln",
+                "Für ein Feuerwerk nach eigenem Gusto",
+                "https://www.mega-waffen-softair-shop.de/media/image/2f/6b/7d/SoftairGranaten.jpg"
+        );
+
+        Trip t8 = new Trip(
+                "Free Solo",
+                "Für den ultimativen Kick klettern wir heute mal ohne lästige Sicherheitsgurte",
+                "https://www.globetrotter.de/magazin/wp-content/uploads/sites/2/2020/03/FreeSolo_11_wm.jpg"
+        );
+
+        Trip t9 = new Trip(
+                "Stierkampf",
+                "Lust, einmal ein Held zu sein?",
+                "https://www.rnd.de/resizer/wKHQ4yUanU39oMOel0GYbvy33rw=/894x503/filters:quality(70):format(webp)/cloudfront-eu-central-1.images.arcpublishing.com/madsack/LHKXJZN3YNGA7A2I4YDFWXKPSQ.jpg"
+        );
+
+        Trip t10 = new Trip(
+                "Bore-Out",
+                "Überstehen Sie die mentale Belastung eines Tags des absoluten Nichtstuns? Dem einen oder anderen gruselt's dabei...",
+                "https://oeffentlichespersonal.ch/wp-content/uploads/Mann-gelangweilt-zvinfo-juni-2019-1-1024x576.jpg"
+        );
+
+        return List.of(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10);
     }
 }
